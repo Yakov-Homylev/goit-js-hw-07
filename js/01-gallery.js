e4c6886cdf5e46f75b8bfa5ef1c629fb0c9a3982
Yakov <yakoventii@gmail.com>
2021-09-28 12:07:25 +0300

    Выполнены задания 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBoxEl = document.querySelector('.gallery')
 
-console.log(galleryItems);
+
+const galleryArrayEl = galleryItems.map(image => `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>
+`).join('');
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryArrayEl)
+
+galleryBoxEl.addEventListener("click", openBigImage)
+
+function openBigImage(evt) {
+    
+    evt.preventDefault()
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`)
+
+    instance.show()
+    if (instance.show) {
+        addEventListener('keydown', () => {
+            if (event.code !== "Escape") {
+                return
+            }
+            instance.close()
+        })
+    }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBoxEl = document.querySelector('.gallery')
 
-console.log(galleryItems);
+const galleryArrayEl = galleryItems.map(image => `<div class="gallery__item">
+  <a class="gallery__item" href="${image.original}"">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</div>
+`).join('');
+
+galleryBoxEl.insertAdjacentHTML('afterbegin', galleryArrayEl)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
